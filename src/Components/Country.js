99cedb6b8b57a99c3693755d8bff960eb227bc37
moon gun <who307@naver.com>
2021-02-20 01:29:38 +0900

    코드수정

--- src/Components/Country.js
+++ src/Components/Country.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { faChevronDown, faTimes } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
@@ -29,35 +29,58 @@ export default function Coutry(props) {
       setOpenLang(false);
     }
   }
-  const selectCountry = (e) => {
-    e.preventDefault();
-    const { name, value } = e.target;
-    setDropMenu({
-      ...dropMenu,
-      [name]: value
+
+  // 지역선택과 언어선택시 선택한 값으로 state변경
+  // 버튼태그에 onClick 함수 추가, name 속성 값 설정
+  useEffect(() => {
+
+    let buttonCountry = document.querySelectorAll(".countryDropDown button, .languageDropDown button")
+    // querySelectorAll 로 가져온 nodeList를 forEach문으로 해당 elements를 반환하고 onclick 이벤트 추가
+    buttonCountry.forEach((countryBtn) => {
+      console.log(countryBtn.parentElement.className )
+      countryBtn.onclick = (e) => {
+        const { name, value } = e.target;
+        setDropMenu({
+          ...dropMenu,
+          [name]: value
+        });
+        if (openCountry && !openLang) {
+          countryOpenFunc()
+        } else if (!openCountry && openLang) {
+          languageOpenFunc()
+        } else {
+          countryOpenFunc()
+          languageOpenFunc()
+        }
+      }
+      // forEach문으로 반환한 elements의 부모요소의 className의 따라 name 속성 값 설정
+      if(countryBtn.parentElement.className === "countryDropDown"){
+        countryBtn.name = "country"
+      }else if(countryBtn.parentElement.className === "languageDropDown"){
+        countryBtn.name = "language"
+      }
     });
-    if (openCountry && !openLang) {
-      countryOpenFunc()
-    } else if (!openCountry && openLang) {
-      languageOpenFunc()
-    } else {
-      countryOpenFunc()
-      languageOpenFunc()
-    }
-  };
+  }, [openCountry, openLang]);
+
+
   // Country 컴포넌트 close
   const closeBtn = () => {
     props.setOpenCountry(!props.openCountry);
+    if (openLang || openCountry) {
+      setOpenLang(false);
+      setOpenCountry(false);
+    }
   }
+
   const confirmBtn = () => {
     props.setCountry(dropMenu)
     props.setOpenCountry(!props.openCountry);
     alert(`국가 : ${dropMenu.country} 언어 : ${dropMenu.language} 입니다.`)
-    if (openCountry && !openLang){
+    if (openCountry && !openLang) {
       countryOpenFunc()
-    }else if (!openCountry && openLang){
+    } else if (!openCountry && openLang) {
       languageOpenFunc()
-    }else if (openCountry && openLang) {
+    } else if (openCountry && openLang) {
       countryOpenFunc()
       languageOpenFunc()
     }
@@ -76,18 +99,18 @@ export default function Coutry(props) {
             <div className="selectWrap" onClick={countryOpenFunc}>{dropMenu.country}
               <FontAwesomeIcon style={openCountry ? { transform: "rotate(180deg)" } : { transform: "rotate(0deg)" }} icon={faChevronDown} />
             </div>
-            <div className="countryDropDown" style={openCountry ? props.openCountry ? { zIndex: "99", opacity: "1", transition: "opacity 0.3s" } : { opacity: "0" } : { display: "none" }}>
-              <button name="country" onClick={selectCountry} value="CANADA">CANADA</button>
-              <button name="country" onClick={selectCountry} value="FRANCE">FRANCE</button>
-              <button name="country" onClick={selectCountry} value="GERMANY">GERMANY</button>
-              <button name="country" onClick={selectCountry} value="ITALY">ITALY</button>
-              <button name="country" onClick={selectCountry} value="NETHERLANDS">NETHERLANDS</button>
-              <button name="country" onClick={selectCountry} value="SOUTH KOREA">SOUTH KOREA</button>
-              <button name="country" onClick={selectCountry} value="SPAIN">SPAIN</button>
-              <button name="country" onClick={selectCountry} value="SWEDEN">SWEDEN</button>
-              <button name="country" onClick={selectCountry} value="UNITED KINGDOM">UNITED KINGDOM</button>
-              <button name="country" onClick={selectCountry} value="USA">USA</button>
-              <button name="country" onClick={selectCountry} value="">SHOW ALL</button>
+            <div className="countryDropDown" style={openCountry ? { zIndex: "99" } : { display: "none" }}>
+              <button value="CANADA">CANADA</button>
+              <button value="FRANCE">FRANCE</button>
+              <button value="GERMANY">GERMANY</button>
+              <button value="ITALY">ITALY</button>
+              <button value="NETHERLANDS">NETHERLANDS</button>
+              <button value="SOUTH KOREA">SOUTH KOREA</button>
+              <button value="SPAIN">SPAIN</button>
+              <button value="SWEDEN">SWEDEN</button>
+              <button value="UNITED KINGDOM">UNITED KINGDOM</button>
+              <button value="USA">USA</button>
+              <button value="SHOW ALL">SHOW ALL</button>
             </div>
           </div>
 
@@ -95,9 +118,9 @@ export default function Coutry(props) {
             <div className="selectWrap" onClick={languageOpenFunc}>{dropMenu.language}
               <FontAwesomeIcon style={openLang ? { transform: "rotate(180deg)" } : { transform: "rotate(0deg)" }} icon={faChevronDown} />
             </div>
-            <div className="languageDropDown" style={openLang ? props.openCountry ? { zIndex: "99", opacity: "1", transition: "opacity 0.3s" } : { opacity: "0" } : { display: "none" }}>
-              <button name="language" onClick={selectCountry} value="ENGLISH">ENGLISH</button>
-              <button name="language" onClick={selectCountry} value="SPANISH">SPANISH</button>
+            <div className="languageDropDown" style={openLang ? { zIndex: "99" } : { display: "none" }}>
+              <button value="ENGLISH">ENGLISH</button>
+              <button value="SPANISH">SPANISH</button>
             </div>
           </div>
           <button onClick={confirmBtn} className="button">CONFIRM</button>
